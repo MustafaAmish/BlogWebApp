b6646236614f87cb8bf141c8021689810da856d4
MustafaAmish <blizzard8585@gmail.com>
2019-01-06 22:09:27 +0200

    Add Data time to comment creat

---
+++ Blog.Data/Migrations/20190106200223_AddCommentDate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Blog.Data.Migrations
+{
+    public partial class AddCommentDate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreateOn",
+                table: "Comments",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreateOn",
+                table: "Comments");
+        }
+    }
+}
--- Blog.Models/Comment.cs
+++ Blog.Models/Comment.cs
@@ -7,10 +7,17 @@ namespace Blog.Models
 {
     public class Comment
     {
+        public Comment()
+        {
+            this.CreateOn=DateTime.UtcNow;
+        }
+
         public int Id { get; set; }
 
         public string Content { get; set; }
 
+        public DateTime CreateOn { get; set; }
+        
         public string UserId { get; set; }
         public virtual  User User { get; set; }
 
